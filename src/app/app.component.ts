4a8e1565d5fd45456ce018002a13a04b6b9bc031
christ134 <Chris.MAT360@PROTONMAIL.COM>
2019-11-25 16:35:48 +0530

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // template: `<p>  angulaaaarr    <b>{{title}}</b></p>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  
+}
